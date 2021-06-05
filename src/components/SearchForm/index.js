f91e98b50680dd3d73de5f9189f0fef5f175b5e6
Tammy Gegoski <tgegoski@gmail.com>
2021-06-05 02:18:39 -0400

    set up searchform and searchresults components

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -6,20 +6,20 @@ function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
+        <label htmlFor="employees">Employee Name:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
+          name="employee"
+          list="employees"
           type="text"
           className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
+          placeholder="Type in an employee name to begin"
+          id="employee"
         />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
+        <datalist id="employees">
+          {props.employees.map(employee => (
+            <option value={employee} key={employee} />
           ))}
         </datalist>
         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
